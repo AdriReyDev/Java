aa9f21b36767998a477d400e5ebf00c86b02c725
AdriReyDev <243771752+AdriReyDev@users.noreply.github.com>
2026-01-29 09:06:18 +0100

    Optimiza E08 y añade comentarios

--- UD4_EstructurasDeAlmacenamiento/E08.java
+++ UD4_EstructurasDeAlmacenamiento/E08.java
@@ -13,20 +13,25 @@ public class E08 {
 
         int[] numerosEnteros;
 
+        // Iniciamos en 0 los Arrays a completar
+
         int[] pares = new int[0];
 
         int[] impares = new int[0];
 
-        int par = 0;
-        int imp = 0;
-
         Scanner sc = new Scanner(System.in);
+
+        // El usuario introduce n
 
         System.out.println("Introduce n: ");
 
         int n = sc.nextInt();
 
+        // Ahora Array numerosEnteros tiene longitud n.
+
         numerosEnteros = new int[n];
+
+        // En el bucle se introducen los n valores para cada indice
 
         for (int i = 0; i < numerosEnteros.length; i++) {
 
@@ -34,22 +39,36 @@ public class E08 {
 
             numerosEnteros[i] = sc.nextInt();
 
-            if (numerosEnteros[i] % 2 == 0) {
+            if (numerosEnteros[i] % 2 == 0 && numerosEnteros[i] != 0) {
+
+                // Si el numero introducido es par y no es 0 Array pares crece en 1 indice.
 
                 pares = Arrays.copyOf(pares, pares.length + 1);
 
-                pares[par] = numerosEnteros[i];
+                // Ahora guarda el valor en el ultimo indice de Array pares
 
-                par++;
+                pares[pares.length - 1] = numerosEnteros[i];
 
-            } else {
+            } else if (numerosEnteros[i] % 2 != 0 && numerosEnteros[i] != 0) {
+
+                // Si el numero introducido es impar y no es 0 Array impares crece en 1 indice.
 
                 impares = Arrays.copyOf(impares, impares.length + 1);
 
-                impares[imp] = numerosEnteros[i];
+                // Ahora guarda el valor en el ultimo indice de Array impares
 
-                imp++;
+                impares[impares.length - 1] = numerosEnteros[i];
 
+            } else {
+
+                // Si el numero introducido es 0 se repite la operacion para introducir el valor
+                // del indice.
+
+                System.out.println("El numero introducido no es valido: ");
+
+                System.out.printf("Introduce el %d numero: ", (i + 1));
+
+                numerosEnteros[i] = sc.nextInt();
             }
 
         }
